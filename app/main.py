4f56aedeb194e01ab63e7952a3ef235570093c29
Meryem Ozge Uzun <76754183+mozgeuzun@users.noreply.github.com>
2022-08-10 13:27:16 +0300

    jwt

--- app/main.py
+++ app/main.py
@@ -1,15 +1,26 @@
-
-from fastapi import FastAPI
+from fastapi_signals import signal
+from fastapi import FastAPI,Request,HTTPException
 from .db import db
-api = FastAPI()
+from .config import SECRET_KEY
+import jwt
 
+api = FastAPI()
 @api.get("/")
 async def root():
     return {"message": "Hello World"}
 
-#@api.exception_handler(ValueError)
 @api.get("/ids/{count}")
-def main(count:int):
+def main(req:Request,count:int):
+    token = req.headers["Authorization"].split()[1]
+    if token is not signal:
+        raise HTTPException(
+            status_code=401,
+            detail="Unauthorized"
+            )
+    else:
+        print(token)
+    s = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+    print(s)
     return db.list(count)
 
 
