5387b89116525b51d51da4c5b1101a69428f2b7e
Meryem Ozge Uzun <76754183+mozgeuzun@users.noreply.github.com>
2022-08-11 09:32:08 +0300

    jwt added

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
-
-from fastapi import FastAPI,Request
+from fastapi import FastAPI,Request,HTTPException
 from .db import db
 import jwt
 SECRET_KEY='123456789'
@@ -8,20 +7,26 @@ api = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+def clientValidate(req:Request):
+    if "authorization" in req.headers.keys():
+        jwt_header = req.headers["authorization"]
+        arr = jwt_header.split(" ")
+        if arr[0].lower() == "bearer" and len(arr)==2:
+            try:
+                return jwt.decode(arr[1], SECRET_KEY, algorithms=["HS256"])
+            except:
+                raise HTTPException(status_code=400,detail="Token is invalid")
+        else:
+            raise HTTPException(status_code=400,detail="'Authorization' header must be formatted as 'Bearer <token>'")
+    else:
+        raise HTTPException(status_code=400,detail="Authorization not sent")
+
 @api.get("/ids/{rnd}")
 def main(req:Request,rnd:int):
-    token = req.headers["Authorization"].split()[1]
-    try:
-        s = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
-        print(s)
-        return db.list(rnd)    
-    except:
-        a="ERROR!!"
-        return a
-
+    clientInfo = clientValidate(req)
+    print(clientInfo)
+    return db.list(rnd)
    
-    
-
 @api.get("/image/{id}")
 def image(id:int):
     return db.image(id)
